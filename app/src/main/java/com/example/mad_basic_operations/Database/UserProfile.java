9e2779688d701297dd8c61f21964b43ef5fab60b
IT20230692 <IT20230692@my.sliit.lk>
2022-06-04 12:49:29 +0530

    add database folder

---
+++ app/src/main/java/com/example/mad_basic_operations/Database/UserProfile.java
@@ -0,0 +1,16 @@
+package com.example.mad_basic_operations.Database;
+
+import android.provider.BaseColumns;
+
+public class UserProfile {
+    // To prevent someone from accidentally instantiating the contract class,
+    // make the constructor private.
+    private UserProfile() {}
+
+    /* Inner class that defines the table contents */
+    public static class FeedEntry implements BaseColumns {
+        public static final String TABLE_NAME = "entry";
+        public static final String COLUMN_NAME_TITLE = "title";
+        public static final String COLUMN_NAME_SUBTITLE = "subtitle";
+    }
+}
